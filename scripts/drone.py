a29f637d36c56742338d5c7228be358fe7a440b7
rdiverdi <Rocco.DiVerdi@students.olin.edu>
2016-03-15 14:25:27 -0400

    added support for flight simulator controller

--- scripts/drone.py
+++ scripts/drone.py
@@ -5,12 +5,14 @@ from sensor_msgs.msg import Joy
 from mavros_msgs.msg import BatteryStatus, State, OverrideRCIn, Waypoint
 # from mavros_msgs.srv import CommandBool, SetMode, WaypointPush, WaypointClear, WaypointSetCurrent
 from cv_bridge import CvBridge, CvBridgeError
+from mission_planning.mission_planner import *
 
 joystick = {'arm': 2, 'disarm': 3, 'failsafe': 0, 'auto': 4, 'manual': 5, 'x': 0, 'y': 1, 'z': 3, 'yaw': 2}
 xbox     = {'arm': 0, 'disarm': 1, 'failsafe': 8, 'auto': 2, 'manual': 3, 'x': 3, 'y': 4, 'z': 1, 'yaw': 0}
 rcsim    = {'arm': 2, 'disarm': 2, 'failsafe': 8, 'auto': 2, 'manual': 3, 'x': 0, 'y': 1, 'z': 2, 'yaw': 4}
+rcsim_lim = [[-.707,.653],[-.587,.598],[-.620,.598],[-1,1],[-.772,.631],[-1,1]]
 
-ctrl = xbox # Set this variable to the joystick you are currently using
+ctrl = rcsim # Set this variable to the joystick you are currently using
 
 # This is a helper class that encodes all of the callback functions for a drone
 class Drone(Missions):
@@ -26,6 +28,7 @@ class Drone(Missions):
         self.battery_remaining = 0
         self.old_z = 1000
         self.just_armed = False
+        self.planner_up = False
 
         # ROS publishers
         self.pub_rc = rospy.Publisher('/drone/rc/override', OverrideRCIn, queue_size=10)
@@ -50,9 +53,9 @@ class Drone(Missions):
     def fly_joystick(self, x=0, y=0, z=0, yaw=0):
         if self.buttons:
             # Arm drone
-            if self.buttons[0] and not self.armed:
-                self.publish_rc([1500, 1500, 1000, 1500, 0, 0, 0, 0])
-                self.srv_mode(0, '5')
+            if self.buttons[ctrl['arm']] and not self.armed:
+                self.publish_rc([1500, 1500, 1000, 1500, 0, 65535, 0, 0])
+                self.srv_mode(0, '2')
                 self.srv_arm(True)
                 self.just_armed = True
                 self.armed = True
@@ -60,19 +63,22 @@ class Drone(Missions):
                 print "Arming drone"
 
             # Disarm drone
-            if self.buttons[1] and self.armed and not self.just_armed:
+            if self.buttons[ctrl['disarm']] and self.armed and not self.just_armed:
                 self.srv_arm(False)
                 print "Disarming drone"
 
             # this should probably be dealt with somewhere else, but I'm not sure how
-            #if self.buttons[7]:
-            #    planner = Map_Planner(self)
+            if self.buttons[4] and not self.planner_up:
+                planner = Map_Planner(self)
+                self.planner_up = True
+            if not self.buttons[4] and self.planner_up:
+                self.planner_up = False
 
         if self.armed:
             if not(x): x = 1500 - self.axes[ ctrl['x'] ] * 300
             if not(y): y = 1500 - self.axes[ ctrl['y'] ] * 300
             if not(z): z = 1500 + self.axes[ ctrl['z'] ] * 500
-            if not(yaw): yaw = 1500 - self.axes[ ctrl['yaw'] ] * 300
+            if not(yaw): yaw = 1500 - self.axes[ ctrl['yaw'] ] * 500
 
             if self.just_armed:
                 z = 1000
@@ -95,8 +101,17 @@ class Drone(Missions):
 
     """ Various callback functions for each ROS topic """
     def joy_callback(self, data):
-        self.axes = data.axes
+        self.axes = list(data.axes)
         self.buttons = data.buttons
+        if ctrl == rcsim:
+            self.scale_axes(rcsim_lim)
+
+    def scale_axes(self, limits):
+        for ch in range(len(self.axes)):
+            if self.axes[ch] > 0:
+                self.axes[ch] *= 1./limits[ch][1]
+            else:
+                self.axes[ch] *= -1./limits[ch][0]
 
     def state_callback(self, data):
         self.armed = data.armed
